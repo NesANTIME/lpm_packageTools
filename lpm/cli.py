354bab836a9effb55211f440be5dc0f9c5b9d45f
NesANTIME <jacpketmovies@gmail.com>
2025-12-20 09:49:41 -0500

    P3.5

--- lpm/cli.py
+++ lpm/cli.py
@@ -18,7 +18,7 @@ DATA = { "icon": [ "┬  ┌─┐┌┬┐", "│  ├─┘│││", "┴─
 
 def icon():
     version = verifyVersion()
-    if (version):
+    if (version != False):
         DATA["icon"][3] = f"┴─┘┴  ┴ ┴ [!] Nueva version {version} disponible!"
 
     for i in DATA["icon"]:
